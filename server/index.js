79c9c98c39bec1fa71ae2cada68830cd175348a9
kakashi2812004 <kakashi2812004@gmail.com>
2025-07-04 07:00:09 +0000

    index fix

--- server/index.js
+++ server/index.js
@@ -36,11 +36,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public/public')));
 
 // Catch-all route to serve index.html for any unmatched routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public/public', 'index.html'));
 });
 
 
